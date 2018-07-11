0d186c978b5dd0c70c194bc300b4bbeb5996e7f9
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-07-11 23:41:36 +0300

    Init project

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const puppeteer = require('puppeteer')
+const port = process.env.PORT || 8080
+const validUrl = require('valid-url')
+
+var parseUrl = function(url) {
+  url = decodeURIComponent(url)
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = 'http://' + url
+  }
+
+  return url
+}
+
+app.get('/', function(req, res) {
+  var urlToScreenshot = parseUrl(req.query.url)
+
+  if (validUrl.isWebUri(urlToScreenshot)) {
+    console.log('Screenshotting: ' + urlToScreenshot + '&t=' + req.query.t)
+    ;(async () => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      })
+
+      const page = await browser.newPage()
+      await page.goto(urlToScreenshot + '&t=' + req.query.t)
+      await page.setViewport({ width: 1920, height: 1080 })
+
+      const video = await page.$('.html5-video-player')
+      await page.evaluate(() => {
+        // Hide youtube player controls.
+        let dom = document.querySelector('.ytp-chrome-bottom')
+        dom.style.display = 'none'
+      })
+
+      await video.screenshot().then(function(buffer) {
+        res.setHeader(
+          'Content-Disposition',
+          'attachment;filename="' + urlToScreenshot + '.png"'
+        )
+        res.setHeader('Content-Type', 'image/png')
+        res.send(buffer)
+      })
+
+      await browser.close()
+    })()
+  } else {
+    res.send('Invalid url: ' + urlToScreenshot)
+  }
+})
+
+app.listen(port, function() {
+  console.log('App listening on port ' + port)
+})
